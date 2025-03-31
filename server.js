1f05a52f1787b4fd279210e70fd808bfc8fb1df9
SrArmstrong <cetisaldavalde@gmail.com>
2025-03-31 14:28:42 -0600

    Cors3

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const routes = require('./routes');
 
 server.use(
     cors({
-        origin: ["http://localhost:3000", "https://server1-gb00.onrender.com/", "https://frontserver-1x23086zy-sergio-perez-aldavaldes-projects.vercel.app/"],
+        origin: ["http://localhost:3000", "https://server1-gb00.onrender.com", "https://frontserver-1x23086zy-sergio-perez-aldavaldes-projects.vercel.app"],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"]
